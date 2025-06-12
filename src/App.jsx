22af3ac8d643c5398038657cc739924d7303e70c
Jhojan Galvis <sayrakatalinagalvis@gmail.com>
2025-06-11 19:28:44 -0500

    Otro comentario

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,33 @@
-import Productos from './Producto.jsx'
-import './App.css'
+import Productos from './Producto.jsx';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <div className='app'>
-        <h1 className='titulo'>Lista de Productos</h1>
-          <div className='lista-productos'>
-            <Productos 
-            nombre="Auriculares"
-            precio="120"
-            categoria="Electronica"
-            imagen="Auriculares.png" 
-            />
-          </div>
-          <div className='lista-productos'>
-            <Productos 
-            nombre="Botella Térmica"
-            precio="60"
-            categoria="Hogar"
-            imagen="Agua.png" 
-            />
-          </div>
-          <div className='lista-productos'>
-            <Productos 
-            nombre="Libro de React"
-            precio="90"
-            categoria="Educacion"
-            imagen="Libro.png"
-            />
-          </div>
+    <div className='app'>
+      <h1>Lista de Productos</h1>
+      
+      <div className='lista-productos'>
+        <Productos 
+          nombre="Auriculares"
+          precio="120"
+          categoria="Electronica"
+          imagen="Auriculares1.png" 
+        />
+        <Productos 
+          nombre="Botella Térmica"
+          precio="60"
+          categoria="Hogar"
+          imagen="Agua1.png" 
+        />
+        <Productos 
+          nombre="Libro de React"
+          precio="90"
+          categoria="Educacion"
+          imagen="Libro1.png"
+        />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
